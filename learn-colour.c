phillid/machine-learning

commit c8f89adfc4bc908f045acf0f9d1e6635cc8f0434

Fix memory freeing error, misc tidy, fix normalisation

learn-colour.c
@@ -6,7 +6,7 @@
 #include "window.h"
 #include "vector.h"
 
-#define TRAINING_SIZE 60
+#define TRAINING_SIZE 10
 
 int train();
 void plot(struct vector *v);
@@ -19,9 +19,9 @@ int main(void)
 {
 	SDL_Event e = {0};
 	bool running = false;
-	bool new = false;
 	int i = 0;
 	struct vector *v = NULL;
+	struct vector *closest = NULL;
 
 	/* Start SDL window */
 	wind.title = "Simple machine learning of colours";
@@ -34,21 +34,8 @@ int main(void)
 		return EXIT_FAILURE;
 
 	running = true;
-	new = true;
 	while (running)
 	{
-		if (new)
-		{
-			v = vector_new(3);
-			/* choose a random vector */
-			vector_random_values(v, 0, 255);
-			plot(v);
-			vector_normalise(v);
-			v = vector_closest(data, TRAINING_SIZE, v);
-			printf("I think this is %s\n", v->label);
-			vector_destroy(v);
-			new = false;
-		}
 		SDL_WaitEvent(&e);
 		switch (e.type)
 		{
@@ -66,7 +53,13 @@ int main(void)
 						running = false;
 						break;
 				}
-				new = true;
+				v = vector_new(3);
+				/* choose a random vector */
+				vector_random_values(v, 0, 255);
+				plot(v);
+				closest = vector_closest(data, TRAINING_SIZE, v);
+				printf("I think this is %s\n", closest->label);
+				vector_destroy(v);
 				break;
 			default:
 				break;

commit 6ec4df943de07cd5d3c53b1058ad88dde7957764

Fix regression introduced in c8f89ad

learn-colour.c
@@ -32,7 +32,9 @@ int main(void)
 	if (train() == EXIT_FAILURE)
 		return EXIT_FAILURE;
 
+
 	running = true;
+	show_off(data, TRAINING_SIZE);
 	while (running)
 	{
 		SDL_WaitEvent(&e);